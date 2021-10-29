21f4feaf8566563b2f5f1573cea891b5ad992239
slymn00 <bulutea126@gmail.com>
2021-10-29 20:34:37 +0300

    update

--- src/test/java/utilities/ConfigurationReader.java
+++ src/test/java/utilities/ConfigurationReader.java
@@ -10,7 +10,7 @@ public class ConfigurationReader {
     private static Properties properties;
 
     static {
-        String path="src/configuration.properties";
+        String path="configuration.properties";
 
         try {
             FileInputStream fileInputStream=new FileInputStream(path);
